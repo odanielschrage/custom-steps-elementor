bd10ae33e7da498bd129f589424d69d5a3b9b354
@odanielschrage <162645980+odanielschrage@users.noreply.github.com>
2024-03-07 20:40:17 -0300

    Update custom-steps-for-elementor.php

--- plugins/custom-steps-for-elementor/custom-steps-for-elementor.php
+++ plugins/custom-steps-for-elementor/custom-steps-for-elementor.php
@@ -1,15 +1,21 @@
-php
+<?php
+/**
+ * Plugin Name: Custom Steps for Elementor
+ * Plugin URI:  https://github.com/odanielschrage/custom-steps-elementor
+ * Description: Um widget personalizado para Elementor que cria blocos de etapas conectadas com opções avançadas de personalização.
+ * Version: 1.0.0
+ * Author: Daniel Schrage - @odanielschrage
+ * Author URI: https://github.com/odanielschrage
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: custom-steps-for-elementor
+ * Elementor tested up to: 3.5.0
+ * Elementor Pro tested up to: 3.5.0
+ * WordPress tested up to: 5.9
+ */
 
-  Plugin Name Custom Steps for Elementor
-  Plugin URI  httpsgithub.comodanielschragecustom-steps-elementor
-  Description Um widget personalizado para Elementor que cria blocos de etapas conectadas com opções avançadas de personalização.
-  Version 1.0.0
-  Author Daniel Schrage - @odanielschrage
-  Author URI httpsgithub.comodanielschrage
-  License GPL-2.0+
-  License URI httpwww.gnu.orglicensesgpl-2.0.txt
-  Text Domain custom-steps-for-elementor
-  Elementor tested up to 3.5.0
-  Elementor Pro tested up to 3.5.0
-  WordPress tested up to 5.9
- 
+add_action('elementor/widgets/widgets_registered', function($widgets_manager) {
+    include_once(__DIR__ . '/widgets/class-custom-steps-widget.php');
+
+    $widgets_manager->register(new \Custom_Steps_Widget());
+});
